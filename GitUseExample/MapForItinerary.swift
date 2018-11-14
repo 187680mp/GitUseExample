b2a9b84c493fe63fe39ca8749f5f94f529568fe6
Patel, Milan M <187680@s020rmc148-10.local>
2018-11-14 09:40:57 -0600

    Add new map-for-itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Patel, Milan M on 11/14/18.
+//  Copyright © 2018 Patel, Milan M. All rights reserved.
+//
+
+import Foundation
+
+class MapForItinerary: NSObject {
+    
+}
