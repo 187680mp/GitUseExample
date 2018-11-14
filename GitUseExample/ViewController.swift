706c9fe86d830d28bbcc3cf8eab67d929f39f1e1
Patel, Milan M <187680@s020rmc148-10.local>
2018-11-14 09:01:52 -0600

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Patel, Milan M on 11/14/18.
+//  Copyright © 2018 Patel, Milan M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
