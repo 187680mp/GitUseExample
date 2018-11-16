13cbc9228f9860e63590f27bf4954835843d1964
Patel, Milan M <187680@s020rmc148-10.local>
2018-11-16 10:09:01 -0600

    New buttonClicked

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -16,8 +16,8 @@ class ViewController: UIViewController {
     }
     
     @IBAction func buttonClicked(_ sender: UIButton) {
-        print("This is a Git tutorial")
-    } 
+        print("You finished!")
+    }
 
 
 }
